f9317a441ccd2b2b4f24302aea259d2fae1cd184
Ankit Kumar <ankits.kvmdam@hotmail.com>
2026-03-29 04:28:51 +0900

    fix: animation element padding issue

--- frontend/app/(routes)/dashboard/page.tsx
+++ frontend/app/(routes)/dashboard/page.tsx
@@ -6,7 +6,7 @@ import { useAppStore } from "@/app/_store/app-store";
 export default function Page() {
   const user = useAppStore((store) => store.user);
   return (
-    <div className="flex flex-1 justify-center items-center relative rounded-md overflow-hidden">
+    <div className="flex flex-1 justify-center items-center relative rounded-md overflow-hidden -m-2">
       <div className="text-center z-10 w-full">
         <h1 className="font-bold text-4xl bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r bg-clip-text text-transparent lg:text-6xl">
           Hi, {user?.name}!
@@ -14,7 +14,7 @@ export default function Page() {
         <p className="muted-foreground">What do you want to chat about?</p>
         <ChatInput />
       </div>
-      <div className="absolute z-1 inset-0 opacity-5">
+      <div className="absolute z-1 inset-0 opacity-10">
         <GrainGradient
           width="100%"
           height="100%"
