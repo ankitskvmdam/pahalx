b5d955590dbd80f48e4f6df5405888832d45213a
Ankit Kumar <ankits.kvmdam@hotmail.com>
2026-04-02 00:18:18 +0900

    fix: head

--- backend/alembic/versions/ed1f666cb225_generate_all_tables.py
+++ backend/alembic/versions/ed1f666cb225_generate_all_tables.py
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "ed1f666cb225"
-down_revision: Union[str, Sequence[str], None] = None
+down_revision: Union[str, Sequence[str], None] = ["731ecda86af1"]
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
